0cfe2a9e8f8138e28f6fc874652db89cb506a31c
ikhvichi <irina.khvichiya@t-systems.com>
2020-04-20 06:59:47 +0200

    Домашнее задание lesson20200409 - add "key" value to "City"

--- src/main/java/lesson20200407_location/City.java
+++ src/main/java/lesson20200407_location/City.java
@@ -3,7 +3,13 @@ package main.java.lesson20200407_location;
 public class City {
     public static String name = "default";
     public static int size = 0;
-
+    public static String key = "spb";
+/**
+Добавить поле key(по умолчанию "spb").
+ В методе main создать массив длинной 10 тип City.
+ Заполнить первые 3 элемента экземплярами класса City с именами "msk", "vrn", "spb".
+Вывести на экран все значения key, присутствующие в массиве.
+ */
 
     public City (String newName, int newSize) {
         name = newName;
@@ -15,7 +21,7 @@ public class City {
     {
         return "City{" +
                 "newName='" + name + '\'' +
-                ", newSize=" + size +
+                ", newSize=" + size + '\'' + ", key='" + key +
                 '}';
     }
 }
--- src/main/java/lesson20200407_location/CityConstractor.java
+++ src/main/java/lesson20200407_location/CityConstractor.java
@@ -3,14 +3,17 @@ package main.java.lesson20200407_location;
 public class CityConstractor {
     public static void buildCities() {
 
-        City[] cityArray = new City[7];
-        cityArray[0] = new City("spb", 444);
+        City[] cityArray = new City[10];
+        cityArray[0] = new City("msk", 444);
         cityArray[1] = new City("vrn", 50000);
-        cityArray[2] = new City("paris", 60000);
-        cityArray[3] = new City("moscow", 70000);
+        cityArray[2] = new City("spb", 60000);
+        cityArray[3] = new City("toronto", 70000);
         cityArray[4] = new City("lpzk", 80000);
         cityArray[5] = new City("brnsk", 90000);
         cityArray[6] = new City("omsk", 110000);
+        cityArray[7] = new City("london", 50);
+        cityArray[8] = new City("brat", 60);
+        cityArray[9] = new City("antaliya", 70);
 
         for (int i = 0; i < cityArray.length; i++) {
             // неявно выполняется sheepArray[i].toString();
